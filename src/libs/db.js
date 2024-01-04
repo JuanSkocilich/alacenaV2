3605ab1d5f6a3d4ea2e6d8a0e51f8f1ea6b25546
Juan Skocilich <juan.skocilich@gmail.com>
2024-01-03 22:01:36 -0300

    First commit

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,20 +1,4 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
-import ws from "ws";
-
-dotenv.config();
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-const prisma = new PrismaClient({ adapter });
-
-export default prisma;
-
-/* import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
@@ -24,6 +8,7 @@ const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma; */
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
 
 /* export const db = new PrismaClient(); */
